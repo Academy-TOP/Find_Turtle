e6d21893cff759c972293ff258c00a6edbda65a9
Евгений Тищук <36456429+Jack36VRN@users.noreply.github.com>
2023-03-04 10:51:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+This is the Template Repl for Python with Turtle.
+
+Python with Turtle lets you make graphics easily in Python.
+
+Check out the official docs here: https://docs.python.org/3/library/turtle.html
+"""
+
+
+
+from turtle import *
+
+jungle = "Наши джунгли очень густые и не проходимые. В них много опасных монстров."
+turtle = ""
+turtle = "" + jungle[14] + jungle[15] + jungle[-4] + jungle[15] + jungle[-16] + jungle[1] + jungle[-11] + jungle[1]
+
+print(turtle)
